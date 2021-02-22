4ad7244e6a6c920b8350b9d7f94c6e7128a558e2
kevinnarbas <kevinnarbas@gmail.com>
2021-02-22 09:01:38 -0800

    complete assessments

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,8 +1,10 @@
 // ASSESSMENT 2: Coding practical questions with Jest
-
 // Please read all questions thoroughly
 // Pseudo coding is HIGHLY recommended
 // If you get stuck, please leave comments to help us understand your thought process
+
+// const { it, expect } = require("@jest/globals");
+// const { describe } = require("yargs");
 
 // Add appropriate dependencies to the repository:
 // $ yarn add jest
@@ -27,12 +29,60 @@ var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
 
+// test if something is divisible by three 
+// if it is divisible by three output that it is, if not than output it is not
 
 
+describe('when isDivisible is called', () => {
+  it('returns "9 is divisible by three", when a number divisible by three is provided', () => {
+    //Arrange
+    const randomNumber = 9
+
+    //Act
+    const actualResult = isDivisible(randomNumber)
+
+    //Assert
+    expect(actualResult).toEqual('9 is divisible by three')
+  })
+
+  it('returns "0 is divisible by three", when a number divisible by three is provided', () => {
+    //Arrange
+    const randomNumber = 0
+
+    //Act
+    const actualResult = isDivisible(randomNumber)
+
+    //Assert
+    expect(actualResult).toEqual('0 is divisible by three')
+  })
+
+  it('returns "-7 is not divisible by three", when a number not divisible by three is provided', () => {
+    //Arrange
+    const randomNumber = -7
+
+    //Act
+    const actualResult = isDivisible(randomNumber)
+
+    //Assert
+    expect(actualResult).toEqual('-7 is not divisible by three')
+  })
+});
 
 // b) Create the function that makes the test pass.
 
+// (works)
+// const isDivisible = (number) => {
+//   return (number % 3 === 0) ? `${number} is divisible by three` : `${number} is not divisible by three`
+// }
 
+// trying with less code
+const isDivisible = num => {
+  return `${num} is${(num % 3) ? ' not' : ''} divisible by three`
+}
+
+// console.log(isDivisible(num1))
+// console.log(isDivisible(num2))
+// console.log(isDivisible(num3))
 
 
 
@@ -46,13 +96,40 @@ var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+describe('when capitalize is called', () => {
+  it('return all of the words in array capitalized when passed an array', () => {
+    // Arrange
+    const randomNouns = ["streetlamp", "potAto", "tEEth", "conCluSion", "nephew"]
 
+    //Act
+    const actualResult = capitalize(randomNouns)
 
+    //Assert
+    expect(actualResult).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+  })
 
+  it('return all of the words in array capitalized when passed an array', () => {
+    // Arrange
+    const randomNouns = ["temperature", "database", "chopsticks", "mango", "deduction"]
+
+    //Act
+    const actualResult = capitalize(randomNouns)
+
+    //Assert
+    expect(actualResult).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+  })
+})
 
 // b) Create the function that makes the test pass.
 
+const capitalize = (array) => {
+  return array.map(value => {
+    return value[0].toUpperCase() + value.substring(1, value.length).toLowerCase()
+  })
+}
 
+// console.log(capitalize(randomNouns1))
+// console.log(capitalize(randomNouns2))
 
 
 
@@ -63,32 +140,133 @@ var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction
 var mixedDataArray1 = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 107]
 
-var mixedDataArray1 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
+var mixedDataArray2 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
 // Expected output: [-9, 0, 3, 18, 94]
 
+describe('when numbersSorted is called ', () => {
+  it('returns a sorted array of only numbers going smallest to largest', () => {
+    //Arrange 
+    const mixedDataArray = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"]
 
+    //Act
+    const actualResult = numbersSorted(mixedDataArray)
 
+    //Assert
+    expect(actualResult).toEqual([-8, 0, 8, 46, 59, 107])
+  })
+
+  it('returns a sorted array of only numbers going smallest to largest', () => {
+    //Arrange 
+    const mixedDataArray = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
+
+    //Act
+    const actualResult = numbersSorted(mixedDataArray)
+
+    //Assert
+    expect(actualResult).toEqual([-9, 0, 3, 18, 94])
+  })
+})
 
 
 // b) Create the function that makes the test pass.
 
+const numbersSorted = (array) => {
+  return array.filter(value => {
+    return typeof value === 'number'
+  }).sort((a, b) => {return a - b})
+}
 
 
+// console.log(numbersSorted(mixedDataArray1))
 
 
 // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var vowelTester1 = "learn"
+var vowelTester1 = "leArn"
 // Expected output: 1
 var vowelTester2 = "academy"
 // Expected output: 0
 var vowelTester3 = "challenge"
 // Expected output: 2
 
+describe('when vowelIndex is called', () => {
+  it('returns the index of the first vowel when a string is passed', () => {
+    //Arrange
+    const vowelTester = 'learn'
 
+    //Act
+    const actualResult = vowelIndex(vowelTester)
 
+    //Assert
+    expect(actualResult).toEqual(1)
+  })
+  it('returns the index of the first vowel when a string is passed', () => {
+    //Arrange
+    const vowelTester = 'academy'
 
+    //Act
+    const actualResult = vowelIndex(vowelTester)
+
+    //Assert
+    expect(actualResult).toEqual(0)
+  })
+  it('returns the index of the first vowel when a string is passed', () => {
+    //Arrange
+    const vowelTester = 'challenge'
+
+    //Act
+    const actualResult = vowelIndex(vowelTester)
+
+    //Assert
+    expect(actualResult).toEqual(2)
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+// trying to answer this without the use of regex 
+
+// const vowelIndex = (string) => {
+//   let indexOfFirst = (string.toLowerCase().split('')).map((value, index) => {
+//     if('aeiou'.includes(value)){
+//       return index
+//     } else {
+//       return value
+//     }
+//   })
+//   return indexOfFirst.sort().shift()
+// }
+
+
+// trying refactor with ternary (works)
+// const vowelIndex = (string) => {
+//   let indexOfFirst = (string.toLowerCase().split('')).map((value, index) => {
+//     return ('aeiou'.includes(value)) ? index : value
+//   })
+//   return indexOfFirst.sort().shift()
+// }
+
+// trying to do extra refactoring (works)
+// const vowelIndex = (string) => {
+//   return (string.toLowerCase().split('')).map((value, index) => {
+//     return ('aeiou'.includes(value)) ? index : value
+//   }).sort().shift()
+// }
+
+// didnt work
+// const vowelIndex = (str) => {
+//   return str.toLowerCase().indexOf('a' || 'e' || 'i' || 'o' || 'u')
+// }
+
+// trying different array methods (works)
+const vowelIndex = (str) => {
+  return (str.toLowerCase().split('')).findIndex((value) => {
+    return ('aeiou'.includes(value))
+  })
+}
+
+//for my own testing to see outcomes to try different things
+// console.log(vowelIndex(vowelTester1))
+// console.log(vowelIndex('ffffdscsddddddsscccccwak'))
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Why would you use the method super()?
 
-  Your answer:
+  Your answer: You use the method super() to inherit the properties from the parent class 
 
-  Researched answer:
+  Researched answer: it referes back to the parent constructor and because javascript enforces it to use the 'this' in a constructor
 
 
 
 2. What is a virtual DOM?
 
-  Your answer:
+  Your answer: The Document object model it is like another layer that react can use to change things on the page in real time
 
-  Researched answer:
+  Researched answer: in react it uses the VDOM to manipulate data by us telling react what state of the user interface to be in 
 
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to display "Hello World" in the browser?
 
-  Your answer:
+  Your answer: It needs a render(){} and return () if theres more then one tag to return then the react fragments are needed <></>
 
-  Researched answer:
+  Researched answer: stateless elements are also called presentational components or dumb components and they just  need the render to return jsx to the view
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JSX is a way of writing html inside of a javascript file
 
-  Researched answer:
+  Researched answer: JSX makes it so we sont have to use separate technologies which is helpful to see when we can see the UI in the JS code
 
 
 
 5. What is state in React?
 
-  Your answer:
+  Your answer: State in react is like a container that holds data that we can manipulate 
 
-  Researched answer:
+  Researched answer: state is fully controlled my the component itself
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer:  Hoisting is the process of bringing functions to the top of a js file because js is a top down language and if a function is involked before it is declared then it will throw an error 
 
-  Researched answer:
+  Researched answer: not just functions but variables also,  and variables defined with 'let' and 'const' are hoisted but they can not be used until after they are declared
 
 
 
@@ -58,8 +58,8 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-- React lifecycle methods
-- React props
-- event.preventDefault()
-- DOM events
-- MVC
+- React lifecycle methods - mounting updating and unmounting
+- React props - like html attributes and js arguments 
+- event.preventDefault() - prevents the default action of a DOM event like a page refresh
+- DOM events - stuff that happens on the page like a keystroke or a scroll 
+- MVC - model, view, controller
